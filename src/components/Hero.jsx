2bb45b020192473fb42a7dc445a7c7a6b11f3be5
Mehul Gopal Nyachhyon <aventmehul@gmail.com>
2025-11-20 17:23:57 +0545

    Implement hero scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import { useRef} from 'react'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 import { SplitText } from 'gsap/SplitText'
+import { useMediaQuery } from 'react-responsive'
+
 
 const Hero = () => {
 
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery({maxWidth: 767});
     useGSAP(() => { 
        const heroSplit = new SplitText('.title', {type:'chars, words'});
        const paragraphSplit = new SplitText('.subtitle', {type:'lines'});
@@ -34,8 +39,30 @@ const Hero = () => {
             scrub: true,
 }
        })
-       .to('.left-leaf', {y: -200}, 0)
-       .to('.right-leaf', {y: 200}, 0)
+    //    .to('.left-leaf', {y: -200}, 0)
+    //    .to('.right-leaf', {y: 200}, 0)
+.to('.left-leaf', { rotation: -30, transformOrigin: 'bottom center' }, 0)  // Rotate counter-clockwise
+.to('.right-leaf', { rotation: 30, transformOrigin: 'bottom center'}, 0)   // Rotate clockwise
+
+
+   const startValue = isMobile? 'top 50%' : 'center 60%';
+   const endValue = isMobile? '120% top' : 'bottom top';
+
+   //Video animation timeline
+   const tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: 'video',
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+    }
+   })
+    videoRef.current.onloadedmetadata = () => {
+        tl.to(videoRef.current, {
+            currentTime: videoRef.current.duration
+        })
+    }
     }, []);
   return (
     <>  
@@ -66,6 +93,16 @@ const Hero = () => {
             </div>
          </div>
     </section>
+
+    <div className="video absolute inset-0">
+        <video 
+            ref={videoRef}
+            src="/videos/output.mp4"
+            muted
+            playsInline
+            preload='auto'
+            ></video>
+    </div>
     </>
   )
 }
